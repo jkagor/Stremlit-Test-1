387d78e22c4dade3d505656694df22f7138f9d0b
Joseph Agor <joe.agor@oregonstate.edu>
2021-06-08 17:36:33 -0700

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -71,6 +71,11 @@ option = st.selectbox(
 'You selected: ', option
 
 
+
+
+
+'''
+
 'Starting a long computation...'
 
 # Add a placeholder
@@ -84,3 +89,44 @@ for i in range(100):
   time.sleep(0.1)
 
 '...and now we\'re done!'
+'''
+
+
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
