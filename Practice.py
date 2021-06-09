8f028895f161d6d4361ad3e8cabaf20fc553d1b1
Joseph Agor <joe.agor@oregonstate.edu>
2021-06-08 17:39:58 -0700

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -74,22 +74,22 @@ option = st.selectbox(
 
 
 
-"""
 
-'Starting a long computation...'
+
+#'Starting a long computation...'
 
 # Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+#latest_iteration = st.empty()
+#bar = st.progress(0)
 
-for i in range(100):
+#for i in range(100):
   # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+#  latest_iteration.text(f'Iteration {i+1}')
+#  bar.progress(i + 1)
+#  time.sleep(0.1)
 
-'...and now we\'re done!'
-"""
+#'...and now we\'re done!'
+
 
 
 
